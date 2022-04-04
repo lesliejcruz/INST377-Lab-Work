8f6b56cfadec4ffcb09c928981a205ab55684e4d
lesliecruz7 <leslieochoa354@gmail.com>
2022-04-03 22:55:46 -0400

    update l6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -6,21 +6,20 @@ function getRandomIntInclusive(min, max) {
 
 function restoArrayMake(dataArray) {
   console.log('fired dataHandler');
-  console.table(dataArray);
+  // console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
     return dataArray[restNum];
-  });
-  console.log(listItems);
-  return listItems;
-
   //  range.forEach((item) => {
-  //    console.log('range item', item);
-  //  });
+  //  console.log('range item', item);
+  // });
+  });
+  // console.log(listItems);
+  // return listItems;
 }
 
-function createhtmlList(collection) {
+function createHtmlList(collection) {
   console.log('fired HTML creator');
   console.log(collection);
   const targetList = document.querySelector('.resto-list');
@@ -39,18 +38,15 @@ async function mainEvent() {
   const submit = document.querySelector('.submit_button');
   submit.style.display = 'none';
 
-  const results = await fetch('./api/foodServicesPG'); // This accesses some data from our API
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  console.log(arrayFromJson);
   if (arrayFromJson.data.length > 0) {
-    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-      // it contains all 1,000 records we need
+      submit.style.display = 'block';
       const restoArray = restoArrayMake(arrayFromJson.data);
-      createhtmlList(restoArray);
+      createHtmlList(restoArray);
     });
   }
 }
